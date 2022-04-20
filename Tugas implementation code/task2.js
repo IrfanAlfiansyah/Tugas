1141a1b5acc0f42773906a03ac5882b3de93e179
IrfanAlfiansyah <irfan.alfiansyah1995@gmail.com>
2022-04-20 23:23:03 +0700

    edit

--- Tugas implementation code/task2.js
+++ Tugas implementation code/task2.js
@@ -1,16 +1,19 @@
 const reverseWord = (text) => {
   let word = ""
   let hasil = ""
-  for (let i = 0; i <= text.length-1; i = i+1){
-    if (text[i] !== " "){
+  for (let i = 0; i <= text.length - 1; i = i + 1) {
+    if (text[i] !== " ") {
       word = word + text[i]
     }
-    if (text[i] === " "){
+    if (text[i] === " ") {
       hasil = " " + word + hasil
       word = ""
     }
+    continue;
   }
   hasil = word + hasil
   return hasil
 }
 console.log(reverseWord("Saya Belajar Javascript"))
+
+//continue?
--- Tugas implementation code/task3.js
+++ Tugas implementation code/task3.js
@@ -2,35 +2,33 @@ const arkFood = (harga, voucher, jarak, pajak) => {
   let potonganHarga = 0;
   let ongkir = 0;
   let subTotal = 0;
+  let tax = 0;
 
   //menghitung harga nilai promo
   if (voucher === "ARKAFOOD5") {
     if (harga >= 50000) {
-      let diskon = harga * 0.5;
-      if (diskon <= 50000) {
-        potonganHarga = diskon;
+      potonganHarga = harga * 0.5; {
+        if (potonganHarga > 50000) {
+          potonganHarga = 50000;
+        }
       }
-      if (diskon > 50000) {
-        potonganHarga = 50000;
-      }
-    } else {
+    }
+    else {
       console.log("\nHarus minimal belanja 50000 untuk dapat menggunakan voucher")
     }
   }
   if (voucher === "DITRAKTIRDEMY") {
     if (harga >= 25000) {
-      let diskon = harga * 0.6;
-      if (diskon <= 30000) {
-        potonganHarga = diskon;
+      potonganHarga = harga * 0.6; {
+        if (potonganHarga > 30000) {
+          potonganHarga = 30000;
+        }
       }
-      if (diskon > 30000) {
-        potonganHarga = 30000;
-      }
-    } else {
+    }
+    else {
       console.log("\nHarus minimal belanja 25000 untuk dapat menggunakan voucher")
     }
   }
-
   //menghitung jarak ongkir
   if (jarak <= 2) {
     ongkir = 5000;
@@ -41,20 +39,18 @@ const arkFood = (harga, voucher, jarak, pajak) => {
 
   //menghitung pajak
   if (pajak === true) {
-    pajak = harga * 0.05;
-  } else {
-    pajak = 0;
+    tax = harga * 0.05;
   }
 
-  subTotal = harga - potonganHarga + ongkir + pajak;
+  subTotal = harga - potonganHarga + ongkir + tax;
 
   console.log(`\nHarga = Rp ${harga}`);
   console.log(`Potongan Harga = Rp ${potonganHarga}`);
   console.log(`Biaya Antar = Rp ${ongkir}`);
-  console.log(`Pajak = Rp ${pajak}`);
+  console.log(`Pajak = Rp ${tax}`);
   console.log(`Jumlah Total = Rp ${subTotal}`);
 };
 arkFood(45000, "ARKAFOOD5", 5, true)
 arkFood(75000, "ARKAFOOD5", 5, false)
-arkFood(15000, "DITRAKTIRDEMY", 5, true)
-arkFood(75000, "DITRAKTIRDEMY", 5, false)
+arkFood(10000, "DITRAKTIRDEMY", 5, true)
+arkFood(50000, "DITRAKTIRDEMY", 5, false)
