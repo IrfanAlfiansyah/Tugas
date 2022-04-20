052f1b391c00c149bb8592907d9ee6e7a99e1aaf
IrfanAlfiansyah <irfan.alfiansyah1995@gmail.com>
2022-04-20 12:10:56 +0700

    edit

--- Tugas implementation code/task2.js
+++ Tugas implementation code/task2.js
@@ -1,12 +1,20 @@
 const reverseWord = (text) => {
-  let wordSplit = text.split(" "); //memisahkan kata demi kata pada text
-  console.log("Hasil pemisahan kata = " + wordSplit);
-
-  // membalik kata demi kata
-  hasilReverse = "";
-  for (let i = wordSplit.length; i > 0; i--) {
-    hasilReverse += " " + wordSplit[i - 1];
+  if (typeof text !== "string"){
+    return "Text harus berupa string"
   }
-  console.log(`${hasilReverse}`);
+  let word = ""
+  let hasil = ""
+  for (let i = 0; i <= text.length-1; i = i+1){
+    if (text[i] !== " "){
+      word = word + text[i]
+    }
+    if (text[i] === " "){
+      hasil = " " + word + hasil
+      word = ""
+    }
+  }
+  hasil = word + "" + hasil
+  return hasil
 }
-reverseWord("Saya Belajar Javascript");
+const output = reverseWord("Saya Belajar Javascript")
+console.log(output)
